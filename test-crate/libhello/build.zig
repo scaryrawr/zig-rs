ecb9f29b53c63f3b3066bbee361c8c458ec03cf5
Mike Wallio <michael.wallio@outlook.com>
2024-12-25 01:36:54 +0000

    Setup shared library on linux

--- test-crate/libhello/build.zig
+++ test-crate/libhello/build.zig
@@ -4,7 +4,7 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const libhello = if (b.args.target.contains("linux"))
+    const libhello = if (b.graph.host.result.os.tag == .linux)
         b.addSharedLibrary(.{
             .name = "hello",
             .root_source_file = b.path("hello.zig"),
