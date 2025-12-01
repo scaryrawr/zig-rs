4fbd278eb0bb4cb446cc2d3a21e5af1282e4b420
Mike Wallio <michael.wallio@outlook.com>
2025-11-30 20:46:47 -0500

    Fix build mappings and test-create build (#8)
    * Switch to scaryrawr features
    
    * Modernize build script
    
    * Fix mappings
    
    * Update CI
    
    * Update zig setup action to version 2 and specify version 0.15.2

--- test-crate/libhello/build.zig
+++ test-crate/libhello/build.zig
@@ -3,13 +3,13 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-
-    const libhello = b.addStaticLibrary(.{
-        .name = "hello",
+    const hello_module = b.createModule(.{
         .root_source_file = b.path("hello.zig"),
         .target = target,
         .optimize = optimize,
     });
+
+    const libhello = b.addLibrary(.{ .name = "hello", .linkage = .static, .root_module = hello_module });
 
     switch (optimize) {
         .Debug, .ReleaseSafe => libhello.bundle_compiler_rt = true,
